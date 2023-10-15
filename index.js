b4878ca4f480fe4f632e512f3defb773f587ebf1
Sergey Maslov <sergey.s.maslov@gmail.com>
2023-10-15 12:57:21 +0300

    bugfix

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-import express from 'express';
-import jwt from 'jsonwebtoken';
-import config from 'dotenv';
-import mongoose from 'mongoose';
+import express from "express";
+import jwt from "jsonwebtoken";
+import config from "dotenv";
+import mongoose from "mongoose";
 
-import { registerValidation } from './validations/auth.js'; 
-import checkAuth from './utils/checkAuth.js';
+import { registerValidation } from "./validations/auth.js";
+import checkAuth from "./utils/checkAuth.js";
 
-import * as UserController from './controllers/UserController'
+import * as UserController from "./controllers/UserController.js";
 
 mongoose
-    .connect(processes.env.DB_CONN)
-    .then(() => console.log('DB Ok'))
-    .catch((err) => console.log('DB error', err));
+  .connect(processes.env.DB_CONN)
+  .then(() => console.log("DB Ok"))
+  .catch((err) => console.log("DB error", err));
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello Me');
+app.get("/", (req, res) => {
+  res.send("Hello Me");
 });
 
-app.post('/auth/login', UserController.login);
+app.post("/auth/login", UserController.login);
 
-app.get('/auth/me', checkAuth, UserController.getMe);
+app.get("/auth/me", checkAuth, UserController.getMe);
 
-app.post('/auth/register', registerValidation, UserController.newuser)
-
+app.post("/auth/register", registerValidation, UserController.newuser);
 
 app.listen(processes.env.PORT, (err) => {
-    if (err) {
-        return console.log(err);
-    }
+  if (err) {
+    return console.log(err);
+  }
 
-    console.log('Server OK');
+  console.log("Server OK");
 });
